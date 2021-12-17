b8d302f5e767855d3d0ecd9edced429933d378bf
Sidney Jones <sjones.presence@gmail.com>
2021-12-17 12:32:55 -0500

    removed import of axios

--- src/containers/StoreLocator.js
+++ src/containers/StoreLocator.js
@@ -3,7 +3,7 @@ import Header from '../components/Header';
 import Button from '../components/Button';
 import Map from '../components/Map';
 import mapChooser from "../mapChooser";
-import axios from "axios";
+//import axios from "axios";
 
 class StoreLocator extends Component {
     shops = [{
